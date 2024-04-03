7fc18e219c7eead07f493af0a9f99bf478da5d9b
Wendel <wendel.isaac12@gmail.com>
2024-04-03 13:01:05 -0300

    feat: adicionando carrossel do slick para planos de internet

--- js/script.js
+++ js/script.js
@@ -1,3 +1,37 @@
-$("document").ready(()=>{
-    
+$(document).ready(function(){
+    $('#slide-planos').slick({
+        infinite: true,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: false
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2,
+              arrows: false
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              arrows: false,
+            }
+          }
+        ]
+      });
 })
